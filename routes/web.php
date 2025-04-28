c0f63e4b25711fd1e506a690e1bd83645fb219a4
B Vikas Chandra <48272577+vikaschandrab@users.noreply.github.com>
2025-04-28 20:20:15 +0530

    Added Route

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/health', [\App\Http\Controllers\HealthCheckController::class, 'index']);
-Route::get('/vikas-profile',[\App\Http\Controllers\VikasProfileController::class, 'homePage']);
+Route::domain('vikas.meetmytech.com')->group(function () {
+    Route::get('/vikas', [\App\Http\Controllers\VikasProfileController::class, 'homePage']);
+});
+Route::get('/vikas-profile', [\App\Http\Controllers\VikasProfileController::class, 'homePage']);
